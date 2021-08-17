484873a7e7733f3ee5bebcc2a6abc17979356b48
Kuntal Chandra <chandra.kuntal@gmail.com>
2021-08-17 11:08:10 +0530

    fix formatting

--- palindromic_substrings.py
+++ palindromic_substrings.py
@@ -22,13 +22,14 @@ Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 https://leetcode.com/problems/palindromic-substrings/discuss/105687/Python-Straightforward-with-Explanation-(Bonus-O(N)-solution)
 We perform a "center expansion" among all possible centers of the palindrome.
 
-Let N = len(S). There are 2N-1 possible centers for the palindrome: we could have a center at S[0], between S[0] and S[1], at S[1], between S[1] and S[2], at S[2], etc.
+Let N = len(S). There are 2N-1 possible centers for the palindrome: we could have a center at S[0], between S[0] and S[1], at S[1], between S[1] and S[2], 
+at S[2], etc.
 
 To iterate over each of the 2N-1 centers, we will move the left pointer every 2 times, and the right pointer every 2 times starting with the second (index 1).
 Hence, left = center / 2, right = center / 2 + center % 2.
 
-From here, finding every palindrome starting with that center is straightforward: while the ends are valid and have equal characters, record the answer and expand.
-
+From here, finding every palindrome starting with that center is straightforward: while the ends are valid and have equal characters, record the answer and 
+expand.
 """
 def countSubstrings(self, S):
     N = len(S)
