f3aa448f68e400333fa82788f7005e5a949871f5
kuntalchandra <chandra.kuntal@gmail.com>
2021-08-20 17:37:02 +0530

    remind valid sudoku

--- other_variations/valid_sudoku.py
+++ other_variations/valid_sudoku.py
@@ -11,33 +11,29 @@ from unittest import TestCase
 
 class Solution(object):
     def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
-        rows=[set() for _ in range(9)]
-        cols=[set() for _ in range(9)]
-        boards=[[set() for _ in range(3)] for _ in range(3)]
-        for row, row_ in enumerate(board):
-            for col, num in enumerate(row_):
-                if num != '.':
-                    in_1 = row // 3
-                    in_2 = col // 3
-                    if num in (rows[row] |cols[col] |boards[in_1][in_2]):
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boards = [[set() for _ in range(3)] for _ in range(3)]
+        for row_idx, row_val in enumerate(board):
+            for col_idx, val in enumerate(row_val):
+                if val != ".":
+                    in_1 = row_idx // 3
+                    in_2 = col_idx // 3
+                    if val in (rows[row_idx] | cols[col_idx] | boards[in_1][in_2]):
                         return False
-                    rows[row].add(num)
-                    cols[col].add(num)
-                    boards[in_1][in_2].add(num)
-        return  True
+                    rows[row_idx].add(val)
+                    cols[col_idx].add(val)
+                    boards[in_1][in_2].add(val)
+        return True
 
 
 class TestValidSudoku(TestCase):
     def setUp(self) -> None:
-        self.input = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],
-                      [".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],
-                      ["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],
-                      [".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],
-                      [".",".",".",".","8",".",".","7","9"]]
+        self.input = [["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+                      [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+                      ["4", ".", ".", "8", ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+                      [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+                      [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
 
     def test_valid_sudoku(self):
         obj = Solution()
